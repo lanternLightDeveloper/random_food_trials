864943727d44fc70f0dc9ad010de152b8ed5dfd3
Joel K Rivers <48956906+aTurbulentMind@users.noreply.github.com>
2023-02-22 13:17:38 -0800

    Add files via upload

---
+++ +page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+
+	const { posts } = data;
+
+	console.log(data);
+	console.log(posts);
+
+	function formatDate(date: Date) {
+		return new Intl.DateTimeFormat('en', { dateStyle: 'long' }).format(date);
+	}
+</script>
+
+<h1>Hello friend {posts} {data}</h1>
+
+<hgroup>
+	<h1>{data.posts.title}</h1>
+	<h2>{formatDate(data.posts.createdAt)}</h2>
+</hgroup>
+
+<div class="content">
+	{@html data.posts.content}
+</div>
